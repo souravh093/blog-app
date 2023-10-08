119fbcfe78d73e9e93ca5f8f1c65605809f23def
Sourave halder <souravh093@gmail.com>
2023-10-08 19:11:08 +0600

    add reset functionality

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
 const baseApi = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://blog-app-server-ten.vercel.app"}),
     tagTypes: ["Blogs"],
     endpoints: (builder) => ({})
 })
